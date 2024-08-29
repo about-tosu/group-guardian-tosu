f645e36bac75f487589674b27d2616e610e9e2e5
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-08-30 02:43:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ async def start(bot, update):
 
 To get started, simply add me to your Telegram group and promote me to admin 
 
-Thanks for using Telegram Group Guardian! Let's keep your group safe and respectful. Powered by @about_tosuu""")
+Thanks for using Telegram Group Guardian! Let's keep your group safe and respectful. Powered by @about_tosuu
+InlineKeyboardButton(text=_["Add me to you group ➕"], url="https://t.me/nothing_guardian_bot?startgroup=true")""")
 
 #-----------------------------------------------------------------
 
